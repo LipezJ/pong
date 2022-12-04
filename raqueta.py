d5edb6acddb4069eb5dd1e9d002438c475640ba2
LipezJ <juandavidlipez47@gmail.com>
2022-12-04 13:25:09 -0500

    curses

--- raqueta.py
+++ raqueta.py
@@ -1,45 +1,37 @@
+import curses
+from curses import wrapper
 
 #librerias
 from pytimedinput import timedKey
 import os
 import time
 
-# area variables
-area = [0, 0, 0, 0, 0, 0, 0, '|', '|', 0, 0, 0, 0, 0, 0, 0, ]
+# rows variables
+rows = 15
 movimientos_area = {'w': -1, 'd': 1}
 
-# funciones area de raqueta
-#   imprimir area
-def imprimir_area():
-    os.system('cls')
-    for area_ in area:
-        print(area_)
+def main(stdscr):
+    # Clear screen
+    actual_rows = [rows//2, rows//2+1]
+    while True:
+        stdscr.clear()
+
+        key, timeout = timedKey(allowCharacters='wdq', timeout=0.1, toprint = False)
+
+        if not timeout:
+            if key == 'q':
+                print(exit())
+            elif (key == 'w' and actual_rows[0]) > 0 or (key == 'd' and actual_rows[1] < rows-1):
+                actual_rows = [ actual_rows[0] + movimientos_area[key], actual_rows[1] + movimientos_area[key]]
+            time.sleep(0.1)
         
-#   movimientos
-def mover():
-    nuevo_area = [
-        actual_area[0] + movimientos_area[key], 
-        actual_area[1] + movimientos_area[key]
-    ]
-    area[actual_area[0]] = 0
-    area[actual_area[1]] = 0  
-    area[nuevo_area[0]] = '|'
-    area[nuevo_area[1]] = '|'
+        for row in range(rows):
+            if row == actual_rows[0] or row == actual_rows[1]:
+                stdscr.addstr('|')
+            else:
+                stdscr.addstr(' ')
+            stdscr.addstr(f'\n')
+            
+        stdscr.refresh()
 
-# codigo
-while True:
-    
-    actual_area = [area.index('|')]
-    actual_area.append(actual_area[0]+1)
-
-    key, timeout = timedKey(allowCharacters='wdq', timeout=0.2)
-
-    if not timeout:
-        if key == 'q':
-            print(exit())
-        elif key == 'w' and actual_area[0] > 0:
-            mover()
-        elif key == 'd' and actual_area[1] < len(area)-1:
-            mover()
-        time.sleep(0.1)
-    imprimir_area()
+wrapper(main)
--- tablero.py
+++ tablero.py
@@ -2,24 +2,28 @@ import random as rn
 import os
 import time
 from pytimedinput import timedKey
+import curses
+from curses import wrapper
 
-def imprimir_tablero():
-    os.system('cls')
-    for tablero_ in tablero:
-        print(tablero_)
-    time.sleep(0.4)
-def buscar_uno():
-    for i in range(len(tablero)):
-        if 1 in tablero[i]:
-            return [i, tablero[i].index(1)]
-def posibilidades_():
-    if (actual[0] == 0 and actual[1] == 0) or (actual[0] == 0 and actual[1] == len(tablero[0])-1) or (actual[0] == len(tablero)-1 and actual[1] == 0) or (actual[0] == len(tablero)-1 and actual[1] == len(tablero[0])-1):
+def imprimir_tablero(stdscr, nuevo):
+    stdscr.addstr('x'*tablero[1] + f'\n')
+    for i in range(tablero[0]):
+        if nuevo[0] == i:
+            stdscr.addstr((' '*(nuevo[1]-1)) + 'O' + ' '*(tablero[1]-nuevo[1]))
+        else:
+            stdscr.addstr(' '*tablero[1])
+        stdscr.addstr(f'\n')
+    stdscr.addstr('x'*tablero[1])
+    time.sleep(0.02)
+
+def posibilidades_(actual):
+    if (actual[0] == 0 and actual[1] == 0) or (actual[0] == 0 and actual[1] == tablero[1]-1) or (actual[0] == tablero[0]-1 and actual[1] == 0) or (actual[0] == tablero[0]-1 and actual[1] == tablero[1]-1):
         return [4]
-    if actual[0] == len(tablero)-1:
+    if actual[0] == tablero[0]-1:
         posibilidades_ = [0, 1]
     elif actual[0] == 0:
         posibilidades_ = [2, 3]
-    elif actual[1] == len(tablero[0])-1:
+    elif actual[1] == tablero[1]-1:
         posibilidades_ = [0, 3]
     elif actual[1] == 0:
         posibilidades_ = [1, 2]
@@ -27,77 +31,54 @@ def posibilidades_():
         posibilidades_ = [0,1,2,3]
     return posibilidades_
 
-tablero = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+tablero = [15, 40]
 movimientos = {
     0: [-1, -1], 1: [-1, 1], 
     2: [1, 1], 3: [1, -1]
 }
 movimientos_ = {
-    '-1-1': 0, '-11': 1,
-    '1-1': 3, '11': 2,
-    '00': 4
+    "-1-1": 0, "-11": 1,
+    "1-1": 3, "11": 2,
+    "00": 4
 }
 movimientos__ = {0: 2, 1: 3, 2: 0, 3: 1}
 
-inicio_fila = rn.randint(0, len(tablero)-1)
-inicio_columna = rn.randint(0, len(tablero[0])-1)
+def main(stdscr):
 
-tablero[inicio_fila][inicio_columna] = 1
-actual = [inicio_fila, inicio_columna]
+    nuevo = [rn.randint(0, tablero[0]-1), tablero[1]//2]
+    actual = nuevo
 
-imprimir_tablero()
-while True:
-    key, outtime = timedKey(allowCharacters='q', timeout=0.1)
-    if key == 'q':
-        print(exit())
-    anterior = actual   
-    actual = buscar_uno()
-    
-    #calcular el tipo de movimiento anterior
-    tipo = str(actual[0] - anterior[0]) + str(actual[1] - anterior[1])
-    tipo = movimientos_[tipo]
+    while True:
+        stdscr.clear()
 
-    #ejecutar la funcion para saber cuantas posibilidades hay en el movimiento
-    posibilidades = posibilidades_()
-    
-    #primer movimiento
-    if actual == anterior:
-        posibilidades = rn.choice(posibilidades_())
-        nuevo = movimientos[posibilidades]
-        nuevo = [actual[0] + nuevo[0], actual[1] + nuevo[1]]
-    
-    #esquinas
-    elif posibilidades[0] == 4:
-        nuevo = anterior
+        #teclas
+        key, outtime = timedKey(allowCharacters='q', timeout=0.05, toprint=False)
+        if key == 'q':
+            print(exit())
 
-    #movimientto normal
-    elif len(posibilidades) == 4:
-        nuevo = [actual[0] - anterior[0], actual[1] - anterior[1]]
-        nuevo = [actual[0] + nuevo[0], actual[1] + nuevo[1]]
-    
-    #rebotes
-    else:
-        tipo_ = movimientos__[tipo]
-        posibilidades.remove(tipo_)
-        nuevo = movimientos[posibilidades[0]]
-        nuevo = [actual[0] + nuevo[0], actual[1] + nuevo[1]]
+        anterior = actual
+        actual = nuevo
+        #calcular el tipo de movimiento anterior
+        tipo = movimientos_[str(actual[0] - anterior[0]) + str(actual[1] - anterior[1])]
+        #ejecutar la funcion para saber cuantas posibilidades hay en el movimiento
+        posibilidades = posibilidades_(actual)
 
-    tablero[nuevo[0]][nuevo[1]] = punto
-    tablero[actual[0]][actual[1]] = 0
-    imprimir_tablero()
+        #primer movimiento
+        if actual == anterior:
+            posibilidad = rn.choice(posibilidades)
+            nuevo = [actual[0] + (movimientos[posibilidad][0]), actual[1] + (movimientos[posibilidad][0])]
+        #esquinas
+        elif posibilidades[0] == 4:
+            nuevo = anterior
+        #movimientto normal
+        elif len(posibilidades) == 4:
+            nuevo = [actual[0] + (actual[0] - anterior[0]), actual[1] + (actual[1] - anterior[1])]
+        #rebotes
+        else:
+            posibilidades.remove(movimientos__[tipo])
+            nuevo = [actual[0] + movimientos[posibilidades[0]][0], actual[1] + movimientos[posibilidades[0]][1]]
+
+        imprimir_tablero(stdscr, nuevo)
+        stdscr.refresh()
+
+wrapper(main)
