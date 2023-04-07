62d4763f1035802336f888ab9540ee2e2d12a5c8
LipezJ <juandavidlipez47@gmail.com>
2023-04-06 23:17:55 -0500

    pong.cpp upgrade

--- cpp/pong.cpp
+++ cpp/pong.cpp
@@ -1,236 +1,198 @@
 #include <iostream>
 #include <conio.h>
+#include <ctime>
 #include <windows.h>
-#include <cstdlib>
-#include <vector> 
+#include <vector>
 #include <map>
-#include <time.h>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+
+#define TIME 100
 
 using namespace std;
 
-vector<vector<int>> tablero(20, vector<int> (80, 0));
-vector<int> area1(20, 0), area2(20, 0), posibilidades;
-map<char, int> movimientos_area;
-map<vector<int>, int> movimientos_;
-int movimientos[4][2] = {{-1,-1},{-1,1},{1,1},{1,-1}};
-int movimientos__[4] = {2, 3, 0, 1}, time_;
+int tablero[2] = {15, 40};
 
-void imprimir(int p1, int p2){
-	system("cls");
-	for (int i = 0; i < 20; i++) {
-		cout<<(area1[i] == 1?"|":" ");
-		for (int j = 0; j < 80; j++){
-            if (tablero[i][j] == 1) {
-                cout<<"O";
-            } else if (j == 37 and i == 0) cout<<p1;
-            else if (j == 40 and i == 0) cout<<p2; 
-            else cout<<" ";
+int movimientos[4][2] = {{-1,-1},{-1,1},{1,1},{1,-1}};
+int movimientos__[4] = {2, 3, 0, 1};
+
+map<vector<int>, int> movimientos_ = {
+    {{-1,-1}, 0}, {{-1,1}, 1},
+    {{1,-1}, 3}, {{1,1}, 2},
+    {{0,0}, 4}
+};
+map<int, int> movimientos_area = {
+    {119, -1}, {100, 1},
+    {107, -1}, {109, 1}
+};
+
+int timed_input();
+vector<int> posibilidades_(vector<int> actual);
+void reinicio(vector<int> &nuevo, vector<int> &actual);
+void imprimir(vector<int> actual, vector<int> puntos, vector<int> actual1, vector<int> actual2);
+
+int main() {
+    srand(time(NULL));
+
+    vector<int> actual_rows1 = {tablero[0] / 2, tablero[0] / 2 + 1};
+    vector<int> actual_rows2 = {tablero[0] / 2, tablero[0] / 2 + 1};
+    vector<int> nuevo = {rand() % tablero[0], tablero[1] / 2};
+    vector<int> actual = nuevo;
+    vector<int> anterior;
+    vector<int> puntos = {0, 0};
+    vector<int> posibilidades;
+
+    int tipo, key, posibilidad;
+
+    system("cls");
+
+    while(true) {
+        anterior = actual;
+        actual = nuevo;
+
+        tipo = movimientos_[{actual[0]-anterior[0], actual[1]-anterior[1]}];
+
+        posibilidades = posibilidades_(actual);
+
+        //movimiento de los jugadores
+        key = timed_input();
+        if (key == 113) {
+            return 0;
+        }else if (key == 112) system("pause>nul");
+        if ((key == 119 && actual_rows1[0] > 0) || (key == 100 && actual_rows1[1] < tablero[0])){
+            actual_rows1[0] = actual_rows1[0] + movimientos_area[key];
+            actual_rows1[1] = actual_rows1[1] + movimientos_area[key];
+        } else if ((key == 107 && actual_rows2[0] > 0) || (key == 109 && actual_rows2[1] < tablero[0])){
+            actual_rows2[0] = actual_rows2[0] + movimientos_area[key];
+            actual_rows2[1] = actual_rows2[1] + movimientos_area[key];
         }
-		cout<<(area2[i] == 1?"|":" ")<<endl;
-	}
-	Sleep(time_);
+
+        //puntos
+        if (puntos[0] == 20){
+            system("cls");
+            cout<<"Gano el jugador 1"<<endl;
+            system("pause>nul");
+            return 0;
+        } else if (puntos[1] == 20){
+            system("cls");
+            cout<<"Gano el jugador 2"<<endl;
+            system("pause>nul");
+            return 0;
+        }
+
+        //movimiento de la pelota
+            //primer movimiento
+        if (actual == anterior){
+            posibilidad = posibilidades[rand() % posibilidades.size()];
+            nuevo[0] = actual[0] + movimientos[posibilidad][0];
+            nuevo[1] = actual[1] + movimientos[posibilidad][1];
+        } 
+            // esquinas
+        else if (posibilidades[0] == 4) {
+            if (((actual[0] == actual_rows1[0] || actual[0] == actual_rows1[1]) && (tipo == 0 || tipo == 3)) || ((actual_rows2[0] == actual[0] || actual_rows2[1] == actual[0]) && (tipo == 1 || tipo == 2))) {
+                nuevo = anterior;
+            } else if (!(actual_rows2[0] == actual[0] || actual_rows2[1] == actual[0])){
+                reinicio(nuevo, actual);
+                puntos[0] ++;
+            } else if (!(actual_rows1[0] == actual[0] || actual_rows1[1] == actual[0])){
+                reinicio(nuevo, actual);
+                puntos[1] ++;
+            }
+        }
+            //movimiento normal
+        else if (posibilidades.size() == 4){
+            nuevo[0] = actual[0] + (actual[0] - anterior[0]);
+            nuevo[1] = actual[1] + (actual[1] - anterior[1]);
+            Sleep(0);
+        } 
+            //movimiento con pared
+        else if (posibilidades == vector<int>{1,2} && !(actual_rows1[0] == actual[0] || actual_rows1[1] == actual[0])){
+            reinicio(nuevo, actual);
+            puntos[1] ++;
+        } else if (posibilidades == vector<int>{0,3} && !(actual_rows2[0] == actual[0] || actual_rows2[1] == actual[0])){
+            reinicio(nuevo, actual);
+            puntos[0] ++;
+        }
+            //rebotes
+        else {
+            if (posibilidades == vector<int>{1,2}){
+                if (actual_rows1[0] == actual[0]) {
+                    tipo = 0;
+                } else if (actual_rows1[1] == actual[0]) {
+                    tipo = 3;
+                }
+            }
+            if (posibilidades == vector<int>{0,3}){
+                if (actual_rows2[0] == actual[0]) {
+                    tipo = 1;
+                } else if (actual_rows2[1] == actual[0]) {
+                    tipo = 2;
+                }
+            }
+            Sleep(0);
+            posibilidades.erase(remove(posibilidades.begin(), posibilidades.end(), movimientos__[tipo]), posibilidades.end());
+            nuevo[0] = actual[0] + movimientos[posibilidades[0]][0];
+            nuevo[1] = actual[1] + movimientos[posibilidades[0]][1];
+        }
+        imprimir(nuevo, puntos, actual_rows1, actual_rows2);
+    }
+    return 0;
 }
-void mover(vector<int> actual, int key, int t){
-    if (t == 1){
-        area1[actual[0]] = 0;
-        area1[actual[1]] = 0;
-        area1[actual[0]+movimientos_area[key]] = 1;
-        area1[actual[1]+movimientos_area[key]] = 1;
-    } else if (t == 2) {
-        area2[actual[0]] = 0;
-        area2[actual[1]] = 0;
-        area2[actual[0]+movimientos_area[key]] = 1;
-        area2[actual[1]+movimientos_area[key]] = 1;
+
+//calcular posibilidades
+vector<int> posibilidades_(vector<int> actual){
+    if ((actual[0] == 0 && actual[1] == 0) || (actual[0] == 0 && actual[1] == tablero[1]) || (actual[0] == tablero[0] && actual[1] == 0) || (actual[0] == tablero[0] && actual[1] == tablero[1])){
+        return vector<int>{4};
+    }
+    if (actual[0] == tablero[0]){
+        return vector<int>{0, 1};
+    } else if (actual[0] == 0){
+        return vector<int>{2, 3};
+    } else if (actual[1] == tablero[1]){
+        return vector<int>{0, 3};
+    } else if (actual[1] == 0){
+        return vector<int>{1, 2};
+    } else {
+        return vector<int>{0, 1, 2, 3};
     }
 }
-vector<int> area_pos(vector<int> area){
-	for (int i = 0; i < area1.size();  i++){
-		if (area[i] == 1) return {i, i+1};
-	}
-}
-vector<int> make_vector(int a, int b){
-	return {a, b};
-}
-vector<int> buscar_uno(){
-	for (int i = 0; i < 20; i++){
-		for (int j = 0; j < 80; j++) {
-			if (tablero[i][j] == 1) return {i, j};
-		}
-	}
-	return {0,0};
-}
-vector<int> posibilidades_(vector<int> actual){
-	if ((actual[0] == 0 and actual[1] == 0) or (actual[0] == 0 and actual[1] == 79 or (actual[0] == 19 and actual[1] == 0) or (actual[0] == 19 and actual[1] == 79))) {
-        return {4,0,1};
-    }else if (actual[0] == 19){
-		return {0,1};
-	}else if (actual[0] == 0){
-		return {2,3};
-	}else if (actual[1] == 79){
-		return {0,3};
-	}else if (actual[1] == 0){
-		return {1,2};
-	}else{
-		return {1,2,3,4};
-	}
-}
-int remove(int item){
-	for (int i = 0; i < posibilidades.size(); i++){
-		if (posibilidades[i] == item){
-            posibilidades.erase(posibilidades.begin()+i, posibilidades.begin() + (i+1)); 
-        };
-	}
-	return 10;
-}
+
+//input de teclas
 int timed_input(){
-	int key = 0;
+    int key = 0;
 	if (_kbhit()){
 		key = _getch();
 	}
 	return key;
 }
-vector<int> random(vector<int> actual){
-	tablero[actual[0]][actual[1]] = 0;
-	int inicio_fila = (rand() % 18) + 1;
-	tablero[inicio_fila][39] = 1;
-	return {inicio_fila, 39};
+
+//reinicio de la pelota
+void reinicio(vector<int> &nuevo, vector<int> &actual){
+    nuevo[0] = rand() % tablero[0];
+    nuevo[1] = tablero[1] / 2;
+    actual = nuevo;
+    Sleep(200);
 }
 
-int main(){
-    cout<<"velocidad: ";
-    cin>>time_;
-    srand(time(NULL));
-    system("mode con: cols=82 lines=22");
-    movimientos_.insert(make_pair(make_vector(-1,-1), 0));
-	movimientos_.insert(make_pair(make_vector(-1,1), 1));
-	movimientos_.insert(make_pair(make_vector(1,1), 2));
-	movimientos_.insert(make_pair(make_vector(1,-1), 3));
-	movimientos_.insert(make_pair(make_vector(0,0), 4));
-    movimientos_area.insert(make_pair(119, -1));
-	movimientos_area.insert(make_pair(100, 1));
-    movimientos_area.insert(make_pair(107, -1));
-	movimientos_area.insert(make_pair(109, 1));
-
-    Sleep(200);
-
-    int key, inicio  = (rand() % 18)+1, puntos1 = 0, puntos2 = 0, tipo, tipo_;
-	vector<int> actual = make_vector(inicio, 40), anterior, nuevo, actual_area1, actual_area2, nue(2);
-    bool k = true;
-
-    area1[9] = 1;
-    area1[10] = 1;
-    area2[9] = 1;
-    area2[10] = 1;
-
-    tablero[inicio][40] = 1;
-    
-    while(puntos1 < 6 or puntos2 < 6){
-        if (!k) {
-            tablero[actual[0]][actual[1]] = 0;
-            tablero[(rand() % 17)+2][40] = 1;
-            actual = buscar_uno();
-            nue = actual;
-            k = true;
+//imprimir tablero
+void imprimir(vector<int> actual, vector<int> puntos, vector<int> actual1, vector<int> actual2){
+    printf("\x1b[0;0H");
+    for (int i = 0; i < tablero[1]+3; i++) cout<<"x";
+    cout<<"\n";
+	for (int i = 0; i < tablero[0]+1; i++) {
+        if (i == actual1[0] || i == actual1[1]) cout<<"|";
+        else cout<<" ";
+        for (int j = 0; j < tablero[1]+1; j++){
+            if (j == actual[1] && i == actual[0]) {
+                cout<<"O";
+            } else cout<<" ";
         }
-        while (k){
-            anterior = actual;
-            actual = buscar_uno();
-
-            for (auto x: anterior) cout<<x;
-            cout<<" ";
-            for (auto x: actual) cout<<x;
-            cout<<" ";
-            for (auto x: nue) cout<<x;
-
-            tipo = movimientos_[{actual[0]-anterior[0], actual[1]-anterior[1]}];
-            posibilidades = posibilidades_(actual);
-
-            actual_area1 = area_pos(area1);
-            actual_area2 = area_pos(area2);
-
-            key = timed_input();
-            if (key == 113) {
-                return 0;
-            }else if (key == 112) system("pause>nul");
-
-            if ((key == 119 and actual_area1[0] > 0) or (key == 100 and actual_area1[1] < 19)){
-                mover(actual_area1, key, 1);
-            } else if ((key == 107 and actual_area2[0] > 0) or (key == 109 and actual_area2[1] < 19)){
-                mover(actual_area2, key, 2);
-            }
-
-            if (puntos1 == 20){
-                system("cls");
-                cout<<"Ganaste, jugador 1";
-                Sleep(1000);
-                break;
-                system("cls");
-            }else if (puntos2 == 20) {
-                system("cls");
-                cout<<"Ganaste, jugador 2";
-                Sleep(1000);
-                break;
-                system("cls");
-            }
-
-            if (actual == anterior){
-                int posibilidades__ = posibilidades_(actual)[rand() % posibilidades_(actual).size()];
-                nuevo = {actual[0] + movimientos[posibilidades__][0], movimientos[posibilidades__][1] + actual[1]};
-                
-            }else if (posibilidades[0] == 4){
-                if (actual[0] == actual_area1[0] or actual[0] == actual_area2[1]){
-                    nuevo = anterior;
-                }else if(!(actual[0] == actual_area1[0] or actual[0] == actual_area2[1])){
-                    puntos2 ++;
-                    Sleep(500);
-                    k = false;
-                    cout<<k;
-                    continue;
-                }else if(!(actual[0] == actual_area2[0] or actual[0] == actual_area2[1])){
-                    puntos1 ++;
-                    Sleep(500);
-                    k = false;
-                    cout<<k;
-                    continue;
-                }
-            }else if(posibilidades.size() == 4){
-                nuevo = {actual[0] + (actual[0] - anterior[0]), actual[1] + (actual[1] - anterior[1])};
-                
-            }else if(posibilidades == make_vector(1,2) and !(actual_area1[0] == actual[0] or actual_area1[1] == actual[0])){
-                puntos2 ++;
-                Sleep(500);
-                k = false;
-                cout<<k;
-                continue;
-            }else if(posibilidades == make_vector(0,3) and !(actual_area2[0] == actual[0] or actual_area2[1] == actual[0])){
-                puntos1 ++;
-                Sleep(500);
-                k = false;
-                cout<<k;
-                continue;
-            }else{
-                if(posibilidades == make_vector(1,2)){
-                    if (actual_area1[0] == actual[0]){
-                        tipo = 0;
-                    }else if (actual_area1[1] == actual[0]){
-                        tipo = 3;
-                    }
-                }
-                if(posibilidades == make_vector(0,3)){
-                    if (actual_area2[0] == actual[0]){
-                        tipo = 1;
-                    }else if (actual_area2[1] == actual[0]){
-                        tipo = 2;
-                    }
-                }
-                tipo_ = movimientos__[tipo];
-                remove(tipo_);
-                nuevo = {actual[0] + movimientos[posibilidades[0]][0], actual[1] + movimientos[posibilidades[0]][1]};
-            }
-            tablero[actual[0]][actual[1]] = 0;
-            tablero[nuevo[0]][nuevo[1]] = 1;
-            imprimir(puntos1, puntos2);
-        }
-    }
-    return 0;
+        if (i == actual2[0] || i == actual2[1]) cout<<"|";
+        else cout<<" ";
+        cout<<"\n";
+	}
+    for (int i = 0; i < tablero[1]+3; i++) cout<<"x";
+    cout<<"\n"<<puntos[0]<<" - "<<puntos[1]<<endl;
+	Sleep(TIME);
 }
